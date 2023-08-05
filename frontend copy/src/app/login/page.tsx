f18f43d0833227d754a6d4c60210f2e261cd1e50
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-08-05 16:13:06 -0500

    Copy frontend files

---
+++ frontend copy/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import * as React from "react";
+import Login from "@/components/login";
+
+function Index() {
+  return <Login/>
+}
+export default Index;
---
+++ frontend copy/src/components/header/index.tsx
@@ -0,0 +1,90 @@
+'use client'
+import * as React from "react";
+import ProfileDropdown from "@/components/profile/dropdown";
+import { Menu, Transition } from '@headlessui/react'
+import {Fragment} from "react";
+
+
+const NavBar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
+  const [flyer, setFlyer] = React.useState(false);
+  const [flyerTwo, setFlyerTwo] = React.useState(false);
+
+  return (
+      <>
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+                <button type="button" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                        className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5"></span>
+                  <span className="sr-only">Open main menu</span>
+
+                  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                       stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                  </svg>
+                  <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                       stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                  </svg>
+                </button>
+              </div>
+              <div
+                  className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img className="h-8 w-auto"
+                       src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                       alt="Your Company"/>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    <a href="#"
+                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                    <a href="#"
+                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                    <a href="#"
+                       className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                  </div>
+                </div>
+              </div>
+              <div
+                  className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <a href="#"
+                   className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                   aria-current="page">Sign up</a>
+                <div className="relative ml-3">
+                  <ProfileDropdown/>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          {mobileMenuOpen &&
+
+              <div className="sm:hidden" id="mobile-menu">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              <a href="#"
+                 className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+              <a href="#"
+                 className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+              <a href="#"
+                 className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+            </div>
+          </div>
+          }
+
+
+
+
+        </nav>
+
+      </>
+  );
+};
+
+export default NavBar;
