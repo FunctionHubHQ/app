3cbdda5e1f82668e17da66e6e6659fa58077a443
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-08-05 22:59:51 -0500

    Add chat box

--- ui/src/components/code/editor.tsx
+++ ui/src/components/code/editor.tsx
@@ -7,17 +7,18 @@ import Controls from "@/components/code/controls";
 const stateFields = { history: historyField };
 import { createTheme } from '@uiw/codemirror-themes';
 import { tags as t } from '@lezer/highlight';
+import Chat from "@/components/code/chat";
 
 const myTheme = createTheme({
   theme: 'light',
   settings: {
-    background: '#efefef',
+    background: '#f3f3f3',
     foreground: '#75baff',
     caret: '#5d00ff',
     selection: '#036dd626',
     selectionMatch: '#036dd626',
     lineHighlight: '#8a91991a',
-    gutterBackground: '#efefef',
+    gutterBackground: '#f3f3f3',
     gutterForeground: '#8a919966',
   },
   styles: [
@@ -42,6 +43,7 @@ const myTheme = createTheme({
 function CodeEditor() {
   const [serializedState, setSerializedState] = useState<string | null>(null);
   const [value, setValue] = useState<string>();
+  const [chatMode, setChatMode] = useState(true)
 
   useEffect(() => {
     setSerializedState(localStorage.getItem('editorState'))
@@ -176,110 +178,120 @@ function CodeEditor() {
         <div className="gf-editor-base gf-controls">
         <Controls/>
         </div>
-        <div className="gf-editor-base gf-editor">
-          <CodeMirror
-              theme={myTheme}
-              extensions={[javascript({ typescript: true })]}
-              value={value}
-              initialState={
-                serializedState
-                    ? {
-                      json: JSON.parse(serializedState || ''),
-                      fields: stateFields,
+        {chatMode ? <Chat/> :
+            <>
+              <div className="gf-editor-base gf-editor">
+                <CodeMirror
+                    theme={myTheme}
+                    extensions={[javascript({ typescript: true })]}
+                    value={value}
+                    initialState={
+                      serializedState
+                          ? {
+                            json: JSON.parse(serializedState || ''),
+                            fields: stateFields,
+                          }
+                          : undefined
                     }
-                    : undefined
-              }
-              onChange={(value, viewUpdate) => {
-                localStorage.setItem('codeValue', value);
+                    onChange={(value, viewUpdate) => {
+                      localStorage.setItem('codeValue', value);
 
-                const state = viewUpdate.state.toJSON(stateFields);
-                localStorage.setItem('editorState', JSON.stringify(state));
-              }}
-          />
-        </div>
-        <div className="gf-editor-base gf-editor">
-          <CodeMirror
-              theme={myTheme}
-              extensions={[javascript({ typescript: true })]}
-              value={"// Write your test request here"}
-              initialState={
-                serializedState
-                    ? {
-                      json: JSON.parse(serializedState || ''),
-                      fields: stateFields,
+                      const state = viewUpdate.state.toJSON(stateFields);
+                      localStorage.setItem('editorState', JSON.stringify(state));
+                    }}
+                />
+              </div>
+              <div className="gf-editor-base gf-editor">
+                <div className="gf-test-tag">
+                <span
+                    className="bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">Test Request</span>
+                </div>
+                <div className="gf-test-editor">
+                <CodeMirror
+                    theme={myTheme}
+                    extensions={[javascript({ typescript: true })]}
+                    value={"// Write your test request here"}
+                    initialState={
+                      serializedState
+                          ? {
+                            json: JSON.parse(serializedState || ''),
+                            fields: stateFields,
+                          }
+                          : undefined
                     }
-                    : undefined
-              }
-              onChange={(value, viewUpdate) => {
-                localStorage.setItem('codeValue', value);
+                    onChange={(value, viewUpdate) => {
+                      localStorage.setItem('codeValue', value);
 
-                const state = viewUpdate.state.toJSON(stateFields);
-                localStorage.setItem('editorState', JSON.stringify(state));
-              }}
-          />
-        </div>
-        <div className="gf-editor-base gf-error">
-          <div>
-            <span
-                className="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-red-400 border border-red-400">Error</span>
-          </div>
-            <span>
-              Uncaught Error: There was an error while hydrating. Because the error happened outside of a Suspense boundary, the entire root will switch to client rendering.
-    at updateHostRoot (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:15468:57)
-    at beginWork$1 (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:17338:14)
-    at beginWork (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:25689:14)
-    at performUnitOfWork (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:24540:12)
-            </span>
-        </div>
-        <div className="gf-editor-base gf-stdout">
-          <div>
-            <span
-                className="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-yellow-400 border border-yellow-300">Console</span>
-          </div>
-          <span>
-            - ready started server on 0.0.0.0:3000, url: http://localhost:3000
-warning ../package.json: No license field
-- event compiled client and server successfully in 377 ms (20 modules)
-- wait compiling...
-- event compiled client and server successfully in 118 ms (20 modules)
-- wait compiling /code/page (client and server)...
-- event compiled client and server successfully in 4.1s (3295 modules)
-- warn
-          </span>
-        </div>
-        <div className="gf-editor-base gf-result">
-          <div>
-            <span
-                className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-green-400 border border-green-400">Result</span>
-          </div>
-          <span>
-            {"{\n" +
-                "  \"model\": \"gpt-3.5-turbo\",\n" +
-                "  \"temperature\": 0.7,\n" +
-                "  \"user\": \"u_N6jrfCkk\",\n" +
-                "  \"messages\": [\n" +
-                "    {\n" +
-                "      \"role\": \"user\",\n" +
-                "      \"content\": \"PROMPT: What is the current time and weather in Boston?\\n\\nANSWER:\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"role\": \"function\",\n" +
-                "      \"name\": \"get_city_time_and_weather\",\n" +
-                "      \"content\": {\n" +
-                "        \"location\": \"Boston, MA\",\n" +
-                "        \"temperature\": \"35\",\n" +
-                "        \"forecast\": [\n" +
-                "          \"rainy\",\n" +
-                "          \"windy\"\n" +
-                "        ],\n" +
-                "        \"current_time\": \"August 1st 2023, 9:58:02 pm\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"max_tokens\": 1000\n" +
-                "}"}
-          </span>
-        </div>
+                      const state = viewUpdate.state.toJSON(stateFields);
+                      localStorage.setItem('editorState', JSON.stringify(state));
+                    }}
+                />
+                </div>
+              </div>
+              <div className="gf-editor-base gf-error">
+                <div>
+                  <span
+                      className="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-red-400 border border-red-400">Error</span>
+                </div>
+                  <span>
+                    Uncaught Error: There was an error while hydrating. Because the error happened outside of a Suspense boundary, the entire root will switch to client rendering.
+          at updateHostRoot (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:15468:57)
+          at beginWork$1 (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:17338:14)
+          at beginWork (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:25689:14)
+          at performUnitOfWork (webpack-internal:///(app-client)/./node_modules/next/dist/compiled/react-dom/cjs/react-dom.development.js:24540:12)
+                  </span>
+              </div>
+              <div className="gf-editor-base gf-stdout">
+                <div>
+                  <span
+                      className="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-yellow-400 border border-yellow-300">Console</span>
+                </div>
+                <span>
+                  - ready started server on 0.0.0.0:3000, url: http://localhost:3000
+      warning ../package.json: No license field
+      - event compiled client and server successfully in 377 ms (20 modules)
+      - wait compiling...
+      - event compiled client and server successfully in 118 ms (20 modules)
+      - wait compiling /code/page (client and server)...
+      - event compiled client and server successfully in 4.1s (3295 modules)
+      - warn
+                </span>
+              </div>
+              <div className="gf-editor-base gf-result">
+                <div>
+                  <span
+                      className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-900 dark:text-green-400 border border-green-400">Result</span>
+                </div>
+                <span>
+                  {"{\n" +
+                      "  \"model\": \"gpt-3.5-turbo\",\n" +
+                      "  \"temperature\": 0.7,\n" +
+                      "  \"user\": \"u_N6jrfCkk\",\n" +
+                      "  \"messages\": [\n" +
+                      "    {\n" +
+                      "      \"role\": \"user\",\n" +
+                      "      \"content\": \"PROMPT: What is the current time and weather in Boston?\\n\\nANSWER:\"\n" +
+                      "    },\n" +
+                      "    {\n" +
+                      "      \"role\": \"function\",\n" +
+                      "      \"name\": \"get_city_time_and_weather\",\n" +
+                      "      \"content\": {\n" +
+                      "        \"location\": \"Boston, MA\",\n" +
+                      "        \"temperature\": \"35\",\n" +
+                      "        \"forecast\": [\n" +
+                      "          \"rainy\",\n" +
+                      "          \"windy\"\n" +
+                      "        ],\n" +
+                      "        \"current_time\": \"August 1st 2023, 9:58:02 pm\"\n" +
+                      "      }\n" +
+                      "    }\n" +
+                      "  ],\n" +
+                      "  \"max_tokens\": 1000\n" +
+                      "}"}
+                </span>
+              </div>
+          </>
+        }
         </>
   );
 }
