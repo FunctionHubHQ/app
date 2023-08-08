2d6f3942e80cfb65f5ab2fc1c59c7c0938319180
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-08-08 11:10:44 -0500

    Add dynamic routing

---
+++ ui/src/app/projects/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import * as React from "react";
+import CodeEditor from "@/components/code/editor";
+import Head from "next/head";
+import {BreadCrumbs} from "@/components/breadcrumbs/BreadCrumbs";
+import { FolderOpenOutline } from 'react-ionicons'
+
+function Page() {
+
+  return  <main>
+    <Head>
+      <title>Hi</title>
+    </Head>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12'>
+        <BreadCrumbs />
+        <CodeEditor/>
+      </div>
+    </section>
+  </main>
+}
+export default Page;
