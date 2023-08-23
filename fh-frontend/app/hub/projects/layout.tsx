2ceacb3f83eda0ac6dba718205930fa175b03512
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-08-22 20:33:51 -0500

    Move all protected pages to hub directory

--- fh-frontend/app/hub/projects/layout.tsx
+++ fh-frontend/app/hub/projects/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import RequireLogin from "#/ui/require-login";
 
 export default async function Layout({
   children,
@@ -10,7 +9,7 @@ export default async function Layout({
 
   return (
     <div className="space-y-9">
-      <div><RequireLogin>{children}</RequireLogin></div>
+      <div>{children}</div>
     </div>
   );
 }
