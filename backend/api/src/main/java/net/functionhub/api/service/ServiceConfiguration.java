b66362f4121d8eb228fecde07d78e290ec0f34e5
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-08-29 20:40:12 -0500

    Remove RabbitMQ

--- backend/api/src/main/java/net/functionhub/api/service/ServiceConfiguration.java
+++ backend/api/src/main/java/net/functionhub/api/service/ServiceConfiguration.java
@@ -3,14 +3,10 @@ package net.functionhub.api.service;
 
 import net.functionhub.api.data.DataConfiguration;
 import net.functionhub.api.service.mapper.MapperConfiguration;
-import net.functionhub.api.service.messaging.Consumer;
 import net.functionhub.api.utils.migration.FlywayMigrationConfiguration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -27,29 +23,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @RequiredArgsConstructor
 @EnableAsync
 @Import({
-    RabbitMQConfiguration.class,
     DataConfiguration.class,
     MapperConfiguration.class,
     OpenAIConfiguration.class,
     FlywayMigrationConfiguration.class})
 public class ServiceConfiguration {
-  private final Consumer consumer;
-
-  @Bean
-  @Qualifier("scrape-job-message-listener")
-  public MessageListenerAdapter scrapeJobConsumer() {
-    return new MessageListenerAdapter(consumer, "scrapeJobConsumer");
-  }
-
-  @Bean
-  @Qualifier("chat-job-message-listener")
-  public MessageListenerAdapter chatJobConsumer() {
-    return new MessageListenerAdapter(consumer, "chatJobConsumer");
-  }
-
-  @Bean
-  @Qualifier("smart-proxy-job-message-listener")
-  public MessageListenerAdapter smartProxyJobConsumer() {
-    return new MessageListenerAdapter(consumer, "smartProxyJobConsumer");
-  }
 }
