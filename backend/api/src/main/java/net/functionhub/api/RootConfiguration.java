c7adc7feff24e6ffdbba1c0f9313c52c314bd32a
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-09-04 19:23:01 -0500

    Use virtual threads to handle servlet requests

--- backend/api/src/main/java/net/functionhub/api/RootConfiguration.java
+++ backend/api/src/main/java/net/functionhub/api/RootConfiguration.java
@@ -1,5 +1,6 @@
 package net.functionhub.api;
 
+import java.util.concurrent.Executors;
 import net.functionhub.api.controller.ControllerConfiguration;
 import net.functionhub.api.data.DataConfiguration;
 import net.functionhub.api.props.PropConfiguration;
@@ -11,8 +12,13 @@ import net.functionhub.api.utils.migration.FlywayPostgresMigration;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 
 /**
  * @author Bizuwork Melesse
@@ -40,5 +46,17 @@ public class RootConfiguration {
     if (!sourceProps.getProfile().equals("prod")) {
 //      seedData.generateSeedData();
     }
+  }
+
+  @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+  public AsyncTaskExecutor asyncTaskExecutor() {
+    return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+  }
+
+  @Bean
+  public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+    return protocolHandler -> {
+      protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    };
   }
 }
