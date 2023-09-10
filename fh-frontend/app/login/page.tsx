fc87426d70411f855a624fa94ec55e86e4cc5850
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2023-09-10 13:59:39 -0500

    Add other sign in buttons

--- fh-frontend/app/login/page.tsx
+++ fh-frontend/app/login/page.tsx
@@ -93,7 +93,17 @@ export default function Page() {
                       d="M5 4.63601C5 3.76031 5.24219 3.1054 5.64323 2.67357C6.03934 2.24705 6.64582 1.9783 7.5014 1.9783C8.35745 1.9783 8.96306 2.24652 9.35823 2.67208C9.75838 3.10299 10 3.75708 10 4.63325V5.99999H5V4.63601ZM4 5.99999V4.63601C4 3.58148 4.29339 2.65754 4.91049 1.99307C5.53252 1.32329 6.42675 0.978302 7.5014 0.978302C8.57583 0.978302 9.46952 1.32233 10.091 1.99162C10.7076 2.65557 11 3.57896 11 4.63325V5.99999H12C12.5523 5.99999 13 6.44771 13 6.99999V13C13 13.5523 12.5523 14 12 14H3C2.44772 14 2 13.5523 2 13V6.99999C2 6.44771 2.44772 5.99999 3 5.99999H4ZM3 6.99999H12V13H3V6.99999Z"
                       fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
                 </svg>
-                <span>Sign in with SSO</span></button>
+                <span>Sign in with Microsoft</span></button>
+
+              <button
+                  className="mx-auto mt-2 flex h-[46px] w-full items-center justify-center space-x-2 rounded-md bg-zinc-900 p-2 text-zinc-500 transition-colors hover:border-gray-400 hover:bg-zinc-800 hover:text-zinc-300 focus:outline-none focus:ring-4 focus:ring-gray-400 focus:ring-opacity-25 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-gray-200 disabled:hover:bg-transparent disabled:hover:text-gray-500">
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
+                     xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-zinc-300">
+                  <path
+                      d="M5 4.63601C5 3.76031 5.24219 3.1054 5.64323 2.67357C6.03934 2.24705 6.64582 1.9783 7.5014 1.9783C8.35745 1.9783 8.96306 2.24652 9.35823 2.67208C9.75838 3.10299 10 3.75708 10 4.63325V5.99999H5V4.63601ZM4 5.99999V4.63601C4 3.58148 4.29339 2.65754 4.91049 1.99307C5.53252 1.32329 6.42675 0.978302 7.5014 0.978302C8.57583 0.978302 9.46952 1.32233 10.091 1.99162C10.7076 2.65557 11 3.57896 11 4.63325V5.99999H12C12.5523 5.99999 13 6.44771 13 6.99999V13C13 13.5523 12.5523 14 12 14H3C2.44772 14 2 13.5523 2 13V6.99999C2 6.44771 2.44772 5.99999 3 5.99999H4ZM3 6.99999H12V13H3V6.99999Z"
+                      fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+                </svg>
+                <span>Sign in with Twitter</span></button>
 
               <div className="py-8 text-center">
                 <span className="text-xs font-light text-zinc-400">
